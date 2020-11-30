5a3fe864c62d15f6cdb7b992d0cc21974be89cd7
Vultum Volubiles <afuau@yandex.ua>
2020-11-30 21:21:09 +0300

    INIT

---
+++ src/test/java/com/vultum/dreams_notebook/DreamsNotebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vultum.dreams_notebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamsNotebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

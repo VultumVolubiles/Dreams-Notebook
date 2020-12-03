0259334b2f6a934352f168ae614eb5672c135d1e
Vultum Volubiles <afuau@yandex.ua>
2020-12-04 02:39:28 +0300

    ADD: User and Role entities

---
+++ src/main/java/com/vultum/dreams_notebook/entity/Role.java
@@ -0,0 +1,22 @@
+package com.vultum.dreams_notebook.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+
+}
---
+++ src/main/java/com/vultum/dreams_notebook/entity/User.java
@@ -0,0 +1,28 @@
+package com.vultum.dreams_notebook.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String login;
+
+    @Column
+    private String name;
+
+    @Column
+    private String password;
+
+    // TODO roles
+}

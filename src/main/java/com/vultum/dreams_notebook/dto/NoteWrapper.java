e5f5341609e6725eb62f5584763b9a753608618f
Vultum Volubiles <afuau@yandex.ua>
2020-12-07 04:31:40 +0300

    ADD: note wrapper

---
+++ src/main/java/com/vultum/dreams_notebook/dto/NoteWrapper.java
@@ -0,0 +1,32 @@
+package com.vultum.dreams_notebook.dto;
+
+import com.vultum.dreams_notebook.entity.Note;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class NoteWrapper extends Wrapper<Note> {
+    private Long id;
+    private String title;
+    private String text;
+    private UserWrapper author;
+
+    @Override
+    public void toWrapper(Note item) {
+        if (item != null) {
+            id = item.getId();
+            title = item.getTitle();
+            text = item.getText();
+            author = new UserWrapper(item.getAuthor());
+        }
+    }
+
+    @Override
+    public void fromWrapper(Note item) {
+        if (item != null) {
+            item.setTitle(title);
+            item.setText(text);
+        }
+    }
+}

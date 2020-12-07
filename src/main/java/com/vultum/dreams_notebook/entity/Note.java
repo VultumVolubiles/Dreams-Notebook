869d1974b1881001018f36aec318dbb747572de8
Vultum Volubiles <afuau@yandex.ua>
2020-12-07 03:52:35 +0300

    ADD: note entity

---
+++ src/main/java/com/vultum/dreams_notebook/entity/Note.java
@@ -0,0 +1,22 @@
+package com.vultum.dreams_notebook.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "notes")
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column String text;
+}

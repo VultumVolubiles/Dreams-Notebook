9e6928df9d29a94d34646e3c833a29c342cd6d71
Vultum Volubiles <afuau@yandex.ua>
2020-12-09 02:34:56 +0300

    ADD: user details service

---
+++ src/main/java/com/vultum/dreams_notebook/service/UserDetailService.java
@@ -0,0 +1,28 @@
+package com.vultum.dreams_notebook.service;
+
+import com.vultum.dreams_notebook.dto.auth.UserDetail;
+import com.vultum.dreams_notebook.entity.User;
+import com.vultum.dreams_notebook.repository.UserRepository;
+import com.vultum.dreams_notebook.utils.Asserts;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailService implements UserDetailsService {
+    private final UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if (username == null || username.isEmpty())
+            throw new IllegalAccessError("Username null or empty string");
+
+        User user = repository.findByLogin(username);
+        Asserts.notNull(user, "User not found");
+
+        return new UserDetail(user);
+    }
+}

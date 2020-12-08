973840d0e9c7704cfa530ac583afece2f33b0034
Vultum Volubiles <afuau@yandex.ua>
2020-12-08 16:28:25 +0300

    UPD: find roles by names in list

--- src/main/java/com/vultum/dreams_notebook/repository/RoleRepository.java
+++ src/main/java/com/vultum/dreams_notebook/repository/RoleRepository.java
@@ -3,8 +3,13 @@ package com.vultum.dreams_notebook.repository;
 import com.vultum.dreams_notebook.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Set;
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
     Role findByName(String name);
+
+    Set<Role> findAllByNameIn(List<String> names);
 
 }

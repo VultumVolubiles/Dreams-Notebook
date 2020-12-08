ff72f14849fc00ed3bea94c398fd2ba37bf30da3
Vultum Volubiles <afuau@yandex.ua>
2020-12-08 16:29:27 +0300

    UPD: set roles to new user

--- src/main/java/com/vultum/dreams_notebook/service/UserService.java
+++ src/main/java/com/vultum/dreams_notebook/service/UserService.java
@@ -1,16 +1,23 @@
 package com.vultum.dreams_notebook.service;
 
+import com.vultum.dreams_notebook.dto.RoleWrapper;
 import com.vultum.dreams_notebook.dto.UserWrapper;
+import com.vultum.dreams_notebook.entity.Role;
 import com.vultum.dreams_notebook.entity.User;
+import com.vultum.dreams_notebook.repository.RoleRepository;
 import com.vultum.dreams_notebook.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository repository;
+    private final RoleRepository roleRepository;
 
     public UserWrapper getById(Long id) {
         Assert.notNull(id, "Id is null");
@@ -28,6 +35,13 @@ public class UserService {
         Assert.isNull(user, "User with login \""+wrapper.getLogin()+"\" already created");
 
         user = new User();
+        Set<Role> roles = roleRepository.findAllByNameIn(wrapper.getRoles()
+                .stream()
+                .map(RoleWrapper::getName)
+                .collect(Collectors.toList()));
+        if (roles.size() < wrapper.getRoles().size())
+            throw new NullPointerException("Not all roles founded");
+        user.setRoles(roles);
         wrapper.fromWrapper(user);
         user.setPassword(wrapper.getPassword());
 
